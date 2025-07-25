97fa42b98e289da7a127f98fb3a96aec2c9eef1c
jt-chihara <non>
2025-07-26 00:38:41 +0900

    hoge

--- tests/algorithms.rs
+++ tests/algorithms.rs
@@ -1,29 +1,6 @@
 use rustpack::*;
 
 #[test]
-fn test_bottom_left_no_rotate() {
-    let rects = [
-        Rectangle { width: 4, height: 2 },
-        Rectangle { width: 3, height: 3 },
-        Rectangle { width: 2, height: 5 },
-    ];
-    let res = bottom_left_placement(&rects, 8, 8, false);
-    assert_eq!(res.len(), 3);
-    // 配置位置や回転有無を個別に検証してもよい
-}
-
-#[test]
-fn test_bottom_left_with_rotate() {
-    let rects = [
-        Rectangle { width: 4, height: 2 },
-        Rectangle { width: 2, height: 4 },
-    ];
-    let res = bottom_left_placement(&rects, 4, 4, true);
-    assert_eq!(res.len(), 2);
-    assert!(res.iter().any(|&(_, _, rotated)| rotated));
-}
-
-#[test]
 fn test_maxrects_with_rotate() {
     let rects = [
         Rectangle { width: 5, height: 2 },
